190664fcc44b3fdcc0b8570733193c234e780173
Mihail Sorokin <85826837+mihail228350df@users.noreply.github.com>
2022-07-02 23:01:54 +0300

    build

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from "./App";
-import 'modern-normalize/modern-normalize.css'
+ 
 import './index.css';
 
 import user from './data/social-profile.json';
